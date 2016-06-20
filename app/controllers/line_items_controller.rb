40b22171716000e221c7fbbbb9d0a654d7600fb3
paradox2107 <anirudh.rao.95@gmail.com>
2016-06-20 17:07:58 +0530

    Checkout

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,18 +28,19 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
-        format.js()
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to store_url }
+        format.js
+        format.json { render action: 'show', status: :created, location: @line_item }
       else
-        format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render action: 'new' }
+        format.json { render json: @line_item.errors,
+        status: :unprocessable_entity }
       end
     end
   end
+
 
   # PATCH/PUT /line_items/1
   # PATCH/PUT /line_items/1.json
